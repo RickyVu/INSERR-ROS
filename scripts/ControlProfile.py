a78c3ec5a3898cf0b7310684c192d7a45aa22cf7
RickyVu <rickyvu189@gmail.com>
2024-07-25 06:45:33 +0000

    config requires changing, joystick work in progress

--- scripts/ControlProfile.py
+++ scripts/ControlProfile.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-
+import sys
 import rospy
 import pubsub.json_message
 
 class ControlProfile:
     def __init__(self, max_percentage = 100, formula_modifier = 30, activate = 'A'):
-        self.movement_pub = pubsub.json_message.Publisher('control.movement', queue_size=10)
-        pubsub.json_message.Subscriber('gamepad.movement', self.movement_listener)
-        pubsub.json_message.Subscriber('gamepad.profile', self.profile_listener)
+        self.debug = pubsub.json_message.Publisher('inserr/control/debug', queue_size=10)
+        self.movement_pub = pubsub.json_message.Publisher('inserr/control/movement', queue_size=10)
+        pubsub.json_message.Subscriber('inserr/input/movement', self.movement_listener)
+        pubsub.json_message.Subscriber('inserr/input/profile', self.profile_listener)
         self.max_percentage = int(max_percentage)/100
         self.formula_modifier = float(formula_modifier)
         self.activate = activate
@@ -37,17 +38,22 @@ class ControlProfile:
             TiltFB *= self.max_percentage
             TiltLR *= self.max_percentage
 
-            self.movement_pub.publish({"control_movement": (Strafe, Drive, Yaw, Updown, TiltFB, TiltLR)})
+            self.movement_pub.publish([Strafe, Drive, Yaw, Updown, TiltFB, TiltLR])
+            self.debug.publish([rospy.get_name(), self.max_percentage, self.formula_modifier, self.activate])
 
     def profile_listener(self, message):
         self.profile_change = message["gamepad_profile"]
 
 if __name__ == '__main__':
+    if len(sys.argv)<2:
+        raise SystemError
     try:
         rospy.init_node('ControlProfile', anonymous=True)
-        max_percentage_param = rospy.get_param('~max_percentage', 100)
-        formula_modifier_param = rospy.get_param('~formula_modifier', 30)
-        activate_param = rospy.get_param('~activate', "A")
+        #param_dir = sys.argv[1]
+        node_name = rospy.get_name()
+        max_percentage_param = rospy.get_param(f'{node_name}/max_percentage', 100)
+        formula_modifier_param = rospy.get_param(f'{node_name}/formula_modifier', 30)
+        activate_param = rospy.get_param(f'{node_name}/activate', "A")
         ControlProfile(max_percentage_param, formula_modifier_param, activate_param)
 
         rospy.spin()
--- scripts/InputSimulator.py
+++ scripts/InputSimulator.py
@@ -6,11 +6,11 @@ import pubsub.json_message
 import random
 
 def input_simulator():
-    pub_thruster = pubsub.json_message.Publisher('inserr/control/movement', queue_size=10)
+    pub_thruster = pubsub.json_message.Publisher('inserr/input/movement', queue_size=10)
     rospy.init_node('input_simulator', anonymous=True)
     rate = rospy.Rate(1) # 10hz
     while not rospy.is_shutdown():
-        message = [random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)]
+        message = {"gamepad_movement": [random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)]}
         pub_thruster.publish(message)
         #rospy.loginfo(message)
         rate.sleep()
